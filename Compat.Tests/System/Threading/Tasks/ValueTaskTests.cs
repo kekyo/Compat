8db5293f191f622367543f62230c8b9e2146e6c4
Kouji Matsui <k@kekyo.net>
2022-12-20 18:17:17 +0900

    Partially implemented ValueTask.

---
+++ Compat.Tests/System/Threading/Tasks/ValueTaskTests.cs
@@ -0,0 +1,75 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Compat - Totally makes compatibility layer for .NET platforms.
+// Copyright (c) Kouji Matsui (@kozy_kekyo, @kekyo@mastodon.cloud)
+//
+// Licensed under Apache-v2: https://opensource.org/licenses/Apache-2.0
+//
+////////////////////////////////////////////////////////////////////////////
+
+using NUnit.Framework;
+
+namespace System.Threading.Tasks;
+
+public sealed class ValueTaskTests
+{
+    [Test]
+    public async Task Void()
+    {
+        await new ValueTask();
+    }
+
+    [Test]
+    public async Task CompletedTask()
+    {
+        await new ValueTask(Task.Factory.CompletedTask());
+    }
+
+    [Test]
+    public async Task Exception()
+    {
+        var actual = new ApplicationException();
+        try
+        {
+            await new ValueTask(Task.Factory.FromException(actual));
+            Assert.Fail();
+        }
+        catch (ApplicationException ex)
+        {
+            Assert.AreSame(ex, actual);
+        }
+    }
+
+    [Test]
+    public async Task Value()
+    {
+        var actual = await new ValueTask<int>(111);
+
+        Assert.AreEqual(111, actual);
+    }
+
+    [Test]
+    public async Task ValuedTask()
+    {
+        var actual = await new ValueTask<int>(TaskEx.FromResult(111));
+
+        Assert.AreEqual(111, actual);
+    }
+
+    [Test]
+    public async Task ExceptionT()
+    {
+        var tcs = new TaskCompletionSource<int>();
+        var actual = new ApplicationException();
+        try
+        {
+            tcs.SetException(actual);
+            await new ValueTask<int>(tcs.Task);
+            Assert.Fail();
+        }
+        catch (ApplicationException ex)
+        {
+            Assert.AreSame(ex, actual);
+        }
+    }
+}
---
+++ Compat/System/Threading/Tasks/ValueTask.cs
@@ -0,0 +1,94 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Compat - Totally makes compatibility layer for .NET platforms.
+// Copyright (c) Kouji Matsui (@kozy_kekyo, @kekyo@mastodon.cloud)
+//
+// Licensed under Apache-v2: https://opensource.org/licenses/Apache-2.0
+//
+////////////////////////////////////////////////////////////////////////////
+
+#if NET35 || NET40 || NET45 || NET452 || NETSTANDARD1_3 || NETSTANDARD1_6
+
+using System.Runtime.CompilerServices;
+
+namespace System.Threading.Tasks;
+
+public readonly struct ValueTask
+{
+    private readonly Task? task;
+
+    public ValueTask(Task task) =>
+        this.task = task;
+
+    public ValueTaskAwaiter GetAwaiter() =>
+        new(this.task);
+}
+
+public readonly struct ValueTask<TValue>
+{
+    private readonly TValue value;
+    private readonly Task<TValue>? task;
+
+    public ValueTask(TValue value) =>
+        this.value = value;
+
+    public ValueTask(Task<TValue> task)
+    {
+        this.value = default!;
+        this.task = task;
+    }
+
+    public ValueTaskAwaiter<TValue> GetAwaiter() =>
+        new(this.value, this.task);
+}
+
+public readonly struct ValueTaskAwaiter : INotifyCompletion
+{
+    private readonly Task? task;
+
+    public ValueTaskAwaiter(Task? task) =>
+        this.task = task;
+
+    public bool IsCompleted =>
+        this.task?.IsCompleted ?? true;
+
+    public void OnCompleted(Action continuation) =>
+        continuation();
+
+    void INotifyCompletion.OnCompleted(Action continuation) =>
+        continuation();
+
+    public void GetResult()
+    {
+        if (this.task is { })
+        {
+            this.task.GetAwaiter().GetResult();
+        }
+    }
+}
+
+public readonly struct ValueTaskAwaiter<TValue> : INotifyCompletion
+{
+    private readonly TValue value;
+    private readonly Task<TValue>? task;
+
+    public ValueTaskAwaiter(TValue value, Task<TValue>? task)
+    {
+        this.value = value;
+        this.task = task;
+    }
+
+    public bool IsCompleted =>
+        this.task?.IsCompleted ?? true;
+
+    public void OnCompleted(Action continuation) =>
+        continuation();
+
+    void INotifyCompletion.OnCompleted(Action continuation) =>
+        continuation();
+
+    public TValue GetResult() =>
+        this.task is { } t ? t.GetAwaiter().GetResult() : this.value;
+}
+
+#endif
