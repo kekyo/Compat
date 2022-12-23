d269ae9e16b4ae63e7ad6c65602290afcbfa8927
Kouji Matsui <k@kekyo.net>
2022-12-23 16:38:18 +0900

    Added Unsafe type.

---
+++ Compat.Tests/System/Runtime/CompilerServices/UnsafeTests.cs
@@ -0,0 +1,38 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Compat - Totally makes compatibility layer for .NET platforms.
+// Copyright (c) Kouji Matsui (@kozy_kekyo, @kekyo@mastodon.cloud)
+//
+// Licensed under Apache-v2: https://opensource.org/licenses/Apache-2.0
+//
+////////////////////////////////////////////////////////////////////////////
+
+using NUnit.Framework;
+using System.Globalization;
+
+namespace System.Runtime.CompilerServices;
+
+public sealed class UnsafeTests
+{
+    [Test]
+    public unsafe void AsPointerAsRef()
+    {
+        var expected = Guid.NewGuid();
+
+        var p = Unsafe.AsPointer(ref expected);
+
+        var actual = Unsafe.AsRef<Guid>(p);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public unsafe void SizeOf()
+    {
+        var expected = sizeof(Guid);
+
+        var actual = Unsafe.SizeOf<Guid>();
+
+        Assert.AreEqual(expected, actual);
+    }
+}
---
+++ Compat/System/Runtime/CompilerServices/Unsafe.cs
@@ -0,0 +1,26 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Compat - Totally makes compatibility layer for .NET platforms.
+// Copyright (c) Kouji Matsui (@kozy_kekyo, @kekyo@mastodon.cloud)
+//
+// Licensed under Apache-v2: https://opensource.org/licenses/Apache-2.0
+//
+////////////////////////////////////////////////////////////////////////////
+
+#if NET35 || NET40
+
+namespace System.Runtime.CompilerServices;
+
+public static class Unsafe
+{
+    [MethodImpl(MethodImplOptions.ForwardRef)]
+    public static extern unsafe void* AsPointer<T>(ref T v);
+
+    [MethodImpl(MethodImplOptions.ForwardRef)]
+    public static extern unsafe ref T AsRef<T>(void* p);
+
+    [MethodImpl(MethodImplOptions.ForwardRef)]
+    public static extern unsafe int SizeOf<T>();
+}
+
+#endif
