d54f17b6cd4e0c2b960bdc90d3136f1b5d024b8c
Kouji Matsui <k@kekyo.net>
2022-12-23 17:14:53 +0900

    Added some Unsafe methods.

--- Compat.Tests/System/Runtime/CompilerServices/UnsafeTests.cs
+++ Compat.Tests/System/Runtime/CompilerServices/UnsafeTests.cs
@@ -9,6 +9,8 @@
 
 using NUnit.Framework;
 using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace System.Runtime.CompilerServices;
 
@@ -27,12 +29,74 @@ public sealed class UnsafeTests
     }
 
     [Test]
-    public unsafe void SizeOf()
+    public unsafe void AsRef()
     {
-        var expected = sizeof(Guid);
+        var expected = Guid.NewGuid();
 
-        var actual = Unsafe.SizeOf<Guid>();
+        var actual = Unsafe.AsRef(in expected);
 
         Assert.AreEqual(expected, actual);
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    private struct StructTarget
+    {
+        public int v0;    // 0
+        public byte v1;   // 4
+        public short v2;  // 5
+        public int v3;    // 7
+        public double v4; // 11
+    }
+
+    [Test]
+    public unsafe void SizeOf()
+    {
+        var expected = sizeof(StructTarget);
+
+        var actual = Unsafe.SizeOf<StructTarget>();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public unsafe void As()
+    {
+        object expected = "ABC";
+
+        var actual = Unsafe.As<string>(expected);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public unsafe void AsNull()
+    {
+        object? expected = null;
+
+        var actual = Unsafe.As<string>(expected);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public unsafe void As2TV()
+    {
+        var expected =
+            MethodImplOptions.NoInlining | MethodImplOptions.PreserveSig;
+
+        short v = (short)expected;
+        var actual = Unsafe.As<short, MethodImplOptions>(ref v);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public unsafe void ByteOffset()
+    {
+        var v = new StructTarget();
+
+        var actual = Unsafe.ByteOffset(ref v.v0, ref v.v3);
+
+        Assert.AreEqual(new IntPtr(7), actual);
     }
 }
--- Compat/System/Runtime/CompilerServices/Unsafe.cs
+++ Compat/System/Runtime/CompilerServices/Unsafe.cs
@@ -20,7 +20,19 @@ public static class Unsafe
     public static extern unsafe ref T AsRef<T>(void* p);
 
     [MethodImpl(MethodImplOptions.ForwardRef)]
+    public static extern ref T AsRef<T>(in T from);
+
+    [MethodImpl(MethodImplOptions.ForwardRef)]
     public static extern unsafe int SizeOf<T>();
+
+    [MethodImpl(MethodImplOptions.ForwardRef)]
+    public static extern T As<T>(object? o) where T : class;
+
+    [MethodImpl(MethodImplOptions.ForwardRef)]
+    public static extern ref TTo As<TFrom, TTo>(ref TFrom from);
+
+    [MethodImpl(MethodImplOptions.ForwardRef)]
+    public static extern IntPtr ByteOffset<T>(ref T origin, ref T target);
 }
 
 #endif
